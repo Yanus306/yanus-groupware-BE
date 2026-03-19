eb0fa43762a2f1ace23f447d43774eb38ea5a63f
jyt6640 <jyt6640@gmail.com>
2026-03-20 00:33:11 +0900

    feat: 프로젝트 글로벌 셋팅 완료

---
+++ src/main/java/com/yanus/attendance/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.yanus.attendance.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Yanus Groupware API")
+                        .description("Yanus Groupware 백엔드 API 문서")
+                        .version("v1.0"))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
+    }
+}
+
