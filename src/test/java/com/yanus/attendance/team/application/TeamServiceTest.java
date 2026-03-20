b413d50dbf9c7e99fce26e97e8641de0d5eca369
jyt6640 <jyt6640@gmail.com>
2026-03-20 13:02:59 +0900

    test: 존재하지 않는 팀 ID 조회 시 예외 발생 테스트 추가

--- src/test/java/com/yanus/attendance/team/application/TeamServiceTest.java
+++ src/test/java/com/yanus/attendance/team/application/TeamServiceTest.java
@@ -1,7 +1,10 @@
 package com.yanus.attendance.team.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.yanus.attendance.global.exception.BusinessException;
+import com.yanus.attendance.global.exception.ErrorCode;
 import com.yanus.attendance.team.domain.Team;
 import com.yanus.attendance.team.domain.TeamName;
 import com.yanus.attendance.team.FakeTeamRepository;
@@ -34,5 +37,17 @@ public class TeamServiceTest {
 
         // then
         assertThat(result).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 팀 ID 조회 시 예외 발생")
+    void not_exist_id_error() {
+        // given
+        Long notExistID = 999L;
+
+        // when & then
+        assertThatThrownBy(() -> teamService.findById(notExistID))
+                .isInstanceOf(BusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.NOT_FOUND);
     }
 }
