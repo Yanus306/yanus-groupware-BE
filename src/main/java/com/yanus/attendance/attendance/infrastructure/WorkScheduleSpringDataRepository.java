7ad36cc2be17ba2d308ab24322b70f9eb110ba1d
jyt6640 <jyt6640@gmail.com>
2026-03-24 21:01:15 +0900

    [Feat] #85 - WorkScheduleRepository 팀/전체 조회 메서드 추가

--- src/main/java/com/yanus/attendance/attendance/infrastructure/WorkScheduleSpringDataRepository.java
+++ src/main/java/com/yanus/attendance/attendance/infrastructure/WorkScheduleSpringDataRepository.java
@@ -5,12 +5,17 @@ import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WorkScheduleSpringDataRepository extends JpaRepository<WorkSchedule, Long> {
 
     Optional<WorkSchedule> findByMemberIdAndDayOfWeek(Long memberId, DayOfWeek dayOfWeek);
 
     List<WorkSchedule> findAllByMemberId(Long memberId);
+
+    @Query("SELECT w FROM WorkSchedule w WHERE w.member.team.id = :teamId")
+    List<WorkSchedule> findAllByMemberTeamId(@Param("teamId") Long teamId);
 
     void deleteByMemberIdAndDayOfWeek(Long memberId, DayOfWeek dayOfWeek);
 }
