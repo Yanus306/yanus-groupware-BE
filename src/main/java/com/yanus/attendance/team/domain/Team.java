a052cd1332488d5dcfd644ad2235a11796a89b24
jyt6640 <jyt6640@gmail.com>
2026-03-25 01:22:12 +0900

    [Fix] #91 - Team 엔티티 @Enumerated 어노테이션 제거
    String 타입 필드에 @Enumerated(EnumType.STRING) 잔존으로
    Hibernate AnnotationException 발생하던 문제 수정

--- src/main/java/com/yanus/attendance/team/domain/Team.java
+++ src/main/java/com/yanus/attendance/team/domain/Team.java
@@ -2,8 +2,6 @@ package com.yanus.attendance.team.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +22,6 @@ public class Team {
     @Column(name = "team_id")
     private Long id;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
 
