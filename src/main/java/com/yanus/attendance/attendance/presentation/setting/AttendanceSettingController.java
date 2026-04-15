dc7c139c978c293ff41f5f6bea40a4b7f56dfc69
jyt6640 <jyt6640@gmail.com>
2026-04-15 21:42:33 +0900

    [Refactor] #152 - AttendanceSetting 컨트롤러 수정

--- src/main/java/com/yanus/attendance/attendance/presentation/setting/AttendanceSettingController.java
+++ src/main/java/com/yanus/attendance/attendance/presentation/setting/AttendanceSettingController.java
@@ -7,6 +7,7 @@ import com.yanus.attendance.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
